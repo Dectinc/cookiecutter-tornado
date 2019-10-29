0ab1a9136467c998f32ebe28c9075459747326ac
Chen Shijiang <chenshijiang@evision.ai>
2019-10-29 23:17:38 +0800

    update run.py

--- {{cookiecutter.repo_name}}/run.py
+++ {{cookiecutter.repo_name}}/run.py
@@ -3,28 +3,81 @@
 
 """Basic run script"""
 
-import tornado.autoreload
+import asyncio
+import signal
+import time
+
 import tornado.httpserver
 import tornado.ioloop
-import tornado.options
-import tornado.web
 from tornado.options import options
+from tornado.platform.asyncio import AnyThreadEventLoopPolicy
+import tornado.web
+import tornado.wsgi
 
-from settings import APP_SETTINGS
+from evision.lib.config import ConfigSection
+from evision.lib.log import logutil
 
-from {{cookiecutter.project_slug}}.urls import url_patterns
+from {{ cookiecutter.project_slug }}.urls import url_patterns
+from settings import APP_SETTINGS, CONFIG
 
-class TornadoApplication(tornado.web.Application):
+logger = logutil.get_logger()
 
+_start = time.time()
+
+
+class TornadoInitialization(tornado.web.Application):
     def __init__(self):
         tornado.web.Application.__init__(self, url_patterns, **APP_SETTINGS)
 
+    @staticmethod
+    def start_services():
+        logger.info('[Application] Starting main service...')
+
+        logger.info('[Application] Service started')
+
+    @staticmethod
+    def stop_services():
+        logger.info('[Application] Saving configuration...')
+        CONFIG.save()
+
+        logger.info('[Application] Stopping main service...')
+
+        logger.info('[Application] Service stopped')
+
+
+TORNADO_APP = TornadoInitialization()
+
+
+def signal_handler(signum, frame):
+    logger.info('Stopping server, with signum={}, frame={}', signum, frame)
+    TORNADO_APP.stop_services()
+    tornado.ioloop.IOLoop.instance().stop()
+
+    import sys
+
+    sys.exit()
+
 
 def main():
-    app = TornadoApplication()
-    app.listen(options.port)
-    tornado.ioloop.IOLoop.current().start()
+    if CONFIG.getboolean(ConfigSection.TORNADO, 'auto_starting'):
+        TORNADO_APP.start_services()
+    if not options.port:
+        options.port = CONFIG.getint('project', 'port')
+
+    asyncio.set_event_loop_policy(AnyThreadEventLoopPolicy())
+    http_server = tornado.httpserver.HTTPServer(TORNADO_APP)
+    http_server.listen(options.port)
+    print('Server inited, listening on {}, time elapsed: {:.4f}s'.format(
+        options.port, time.time() - _start))
+    signal.signal(signal.SIGINT, signal_handler)
+
+    tornado.ioloop.IOLoop.instance().start()
 
 
 if __name__ == "__main__":
     main()
+
+__all__ = [
+    'TORNADO_APP',
+    'main'
+]
