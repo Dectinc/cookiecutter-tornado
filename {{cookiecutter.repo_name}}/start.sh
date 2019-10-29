9160ffeef09011efc259cb6cc5d748a9dededf73
Dectinc Chen <chen@shijiang.info>
2019-10-29 21:36:53 +0800

    update starting script

--- {{cookiecutter.repo_name}}/start.sh
+++ {{cookiecutter.repo_name}}/start.sh
@@ -5,6 +5,8 @@ PYTHON=$(which python3)
 LOG_DIR={% raw %}${{% endraw %}{{ cookiecutter.project_slug|upper }}_DIR}/data/logs
 mkdir -p ${LOG_DIR}
 
-#echo ${PYTHON} ${SAMARITAN_DIR}/run.py --profile=nuc --debug=False --log=warn > ${LOG_DIR}/samaritan-`date +'%Y%m%d_%H%M%S'`.log
-nohup ${PYTHON} {% raw %}${{% endraw %}{{ cookiecutter.project_slug|upper }}_DIR}/run.py --profile=dev --debug=False --log=warn > ${LOG_DIR}/{{ cookiecutter.project_slug }}-`date +'%Y%m%d_%H%M%S'`.log 2>&1 &
+if [ -e ${LOG_DIR}/{{ cookiecutter.project_slug }}.log ]; then
+    mv ${LOG_DIR}/{{ cookiecutter.project_slug }}.log ${LOG_DIR}/{{ cookiecutter.project_slug }}-`date +'%Y%m%d_%H%M%S'`.log
+fi
+nohup ${PYTHON} {% raw %}${{% endraw %}{{ cookiecutter.project_slug|upper }}_DIR}/run.py --profile=dev --debug=False --log=warn > ${LOG_DIR}/{{ cookiecutter.project_slug }}.log 2>&1 &
 echo "{{ cookiecutter.project_slug|title }} started"
